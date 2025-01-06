4a0f2ddd553ca9e66825e2f7c60fe5b4f51bdffd
Robert Duggan <robert.duggan@live.com>
2025-01-05 23:42:54 -0500

    retooled java again

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -3,15 +3,8 @@ local jdtls = require("jdtls")
 local set = vim.keymap.set
 --local jdtls = require("mason-registry").get_package("jdtls")
 
--- File types that signify a Java project's root directory. This will be
--- used by eclipse to determine what constitutes a workspace
 local root_markers = { "gradlew", "mvnw", ".git" }
 local root_dir = require("jdtls.setup").find_root(root_markers)
-
--- eclipse.jdt.ls stores project specific data within a folder. If you are working
--- with multiple different projects, each project must use a dedicated data directory.
--- This variable is used to configure eclipse to use the directory name of the
--- current project found using the root_marker as the folder for project specific data.
 local workspace_folder = home .. "/.config/eclipse/workspace/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
 -- get java debug adapter for DAP
@@ -43,8 +36,7 @@ local keys = {
   { "n", "<leader>Ji", cmd = require("springboot-nvim").generate_interface,           "create [i]nterface" },
   { "n", "<leader>Je", cmd = require("springboot-nvim").generate_enum,                "create [e]num" },
 }
--- The on_attach function is used to set key maps after the language server
--- attaches to the current buffer
+
 local on_attach = function(client, bufnr)
   vim.cmd(
     "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)"
@@ -55,7 +47,6 @@ local on_attach = function(client, bufnr)
 
   require("nvim-navic").attach(client, bufnr)
 
-  -- set keymaps from javakeys.lua
   for k, v in ipairs(keys) do
     set(v[1], v[2], v[3], { noremap = true, silent = true, buffer = bufnr, desc = v[4] })
   end
@@ -85,12 +76,8 @@ local config = {
   flags = {
     debounce_text_changes = 80,
   },
-  on_attach = on_attach, -- We pass our on_attach keybindings to the configuration map
-  root_dir = root_dir, -- Set the root directory to our found root_marker
-  -- Here you can configure eclipse.jdt.ls specific settings
-  -- These are defined by the eclipse.jdt.ls project and will be passed to eclipse when starting.
-  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-  -- for a list of options
+  on_attach = on_attach,
+  root_dir = root_dir,
   capabilities = capabilities,
   init_options = {
     bundles = bundles,
@@ -101,9 +88,6 @@ local config = {
       format = {
         enabled = true,
         settings = {
-          -- Use Google Java style guidelines for formatting
-          -- To use, make sure to download the file from https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml
-          -- and place it in the ~/.local/share/eclipse directory
           url = "/.local/share/eclipse/eclipse-java-google-style.xml",
           profile = "GoogleStyle",
         },
@@ -115,8 +99,7 @@ local config = {
         downloadSources = true,
       },
       signatureHelp = { enabled = true },
-      contentProvider = { preferred = "fernflower" }, -- Use fernflower to decompile library code
-      -- Specify any completion options
+      contentProvider = { preferred = "fernflower" },
       completion = {
         favoriteStaticMembers = {
           "org.hamcrest.MatcherAssert.assertThat",
@@ -142,14 +125,12 @@ local config = {
           "org",
         },
       },
-      -- Specify any options for organizing imports
       sources = {
         organizeImports = {
           starThreshold = 9999,
           staticStarThreshold = 9999,
         },
       },
-      -- How code generation should act
       codeGeneration = {
         toString = {
           template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
@@ -159,11 +140,6 @@ local config = {
         },
         useBlocks = true,
       },
-      -- If you are developing in projects with different Java versions, you need
-      -- to tell eclipse.jdt.ls to use the location of the JDK for your Java version
-      -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-      -- And search for `interface RuntimeOption`
-      -- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
       configuration = {
         updateBuildConfiguration = true,
         runtimes = {
@@ -196,11 +172,6 @@ local config = {
     },
   },
 
-  -- cmd is the command that starts the language server. Whatever is placed
-  -- here is what is passed to the command line to execute jdtls.
-  -- Note that eclipse.jdt.ls must be started with a Java version of 17 or higher
-  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-  -- for the full list of options
   cmd = {
     "/Library/Java/JavaVirtualMachines/amazon-corretto-21.jdk/Contents/Home/bin/java",
     "-Declipse.application=org.eclipse.jdt.ls.core.id1",
@@ -214,27 +185,17 @@ local config = {
     "java.base/java.util=ALL-UNNAMED",
     "--add-opens",
     "java.base/java.lang=ALL-UNNAMED",
-    -- If you use lombok, download the lombok jar and place it in ~/.local/share/eclipse
-    "-javaagent:"
-      .. home
-      .. "/.local/share/eclipse/lombok.jar",
+    "-javaagent:" .. home .. "/.local/share/eclipse/lombok.jar",
 
-    -- The jar file is located where jdtls was installed. This will need to be updated
-    -- to the location where you installed jdtls
     "-jar",
     vim.fn.glob("/opt/homebrew/Cellar/jdtls/1.43.0/libexec/plugins/org.eclipse.equinox.launcher_*.jar"),
 
-    -- The configuration for jdtls is also placed where jdtls was installed. This will
-    -- need to be updated depending on your environment
     "-configuration",
     "/opt/homebrew/Cellar/jdtls/1.43.0/libexec/config_mac_arm",
 
-    -- Use the workspace_folder defined above to store data for this project
     "-data",
     workspace_folder,
   },
 }
 
--- Finally, start jdtls. This will run the language server using the configuration we specified,
--- setup the keymappings, and attach the LSP client to the current buffer
 jdtls.start_or_attach(config)
