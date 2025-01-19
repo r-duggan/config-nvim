6be089106499d5a69870f7a2c611e2d9b2c5fb09
Robert Duggan <robert.duggan@live.com>
2025-01-19 17:52:18 -0500

    added checks to maven to prevent starting when opening without a base folder

--- nvim/lua/plugins/java/maven.lua
+++ nvim/lua/plugins/java/maven.lua
@@ -4,19 +4,24 @@ return {
   {
     "eatgrass/maven.nvim",
     cmd = { "Maven", "MavenExec" },
-    dependencies = "nvim-lua/plenary.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
     config = function()
       -- get root of project
       local root_markers = { "gradlew", "mvnw", ".git" }
-      local root_dir = require("jdtls.setup").find_root(root_markers)
-      require("maven").setup({
-        executable = root_dir .. "/mvnw", -- `mvn` should be in your `PATH`, or the path to the maven exectable, for example `./mvnw`
-        cwd = root_dir,
-        settings = nil, -- specify the settings file or use the default settings
-        commands = { -- add custom goals to the command list
-          { cmd = { "clean", "compile" }, desc = "clean then compile" },
-        },
-      })
+      local jdtls = require("jdtls.setup").find_root
+      local success, root_dir = pcall(require, jdtls(root_markers))
+      if success then
+        require("maven").setup({
+          executable = root_dir .. "/mvnw", -- `mvn` should be in your `PATH`, or the path to the maven exectable, for example `./mvnw`
+          cwd = root_dir,
+          settings = nil, -- specify the settings file or use the default settings
+          commands = { -- add custom goals to the command list
+            { cmd = { "clean", "compile" }, desc = "clean then compile" },
+          },
+        })
+      end
     end,
   },
 }
