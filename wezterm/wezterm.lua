d06edb48c1eaa631da0c9b17fe77714dbbcfd305
Robert Duggan <robert.duggan@live.com>
2025-01-21 21:30:54 -0500

    linked zshrc, updated wezterm, updated tmux

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,8 +4,9 @@ local wezterm = require("wezterm")
 -- Hold the configuration
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
+config.font = wezterm.font("Liga SFMono Nerd Font")
 config.font_size = 14
+config.line_height = 1.05
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
